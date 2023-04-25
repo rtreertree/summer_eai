3a3df5414a29744c8339014a530c3ffc318f6b87
rtreertree <rtreertree@gmail.com>
2023-04-25 15:10:08 +0700

    vmvvgbbcj,vh,

--- 25_04_23/w25_04_23_a1.py
+++ 25_04_23/w25_04_23_a1.py
@@ -1,11 +1,9 @@
 number, dashs= [], []
-
 for i in range(9):
     text = ""
     for n in range(1, i+2): text += str(n)
     number.append(text)
     dashs.append("-"*i)
-
 dashs.reverse()
 for text, dash in zip(number, dashs):
     print(dash + text)
