4c0a67c414db324628dc1d52dc0ff8cbd78992e5
rtreertree <rtreertree@gmail.com>
2023-04-25 15:22:34 +0700

    AA

--- 25_04_23/w25_04_23_a2.py
+++ 25_04_23/w25_04_23_a2.py
@@ -1,6 +1,14 @@
-X = []
 for i in range(9):
-    X.append("x"*(i+1))
-X.reverse()
-for layer in X:
-    print(layer)
+    print("x"*(10-(i+1)))
+#OUTPUT 
+"""
+xxxxxxxxx
+xxxxxxxx
+xxxxxxx
+xxxxxx
+xxxxx
+xxxx
+xxx
+xx
+x
+"""
