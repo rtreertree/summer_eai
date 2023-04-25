d13ab4ce3a24c172db4ca6ee16007a0feee94af1
rtreertree <rtreertree@gmail.com>
2023-04-25 15:13:44 +0700

    sdfsfdsf

--- 25_04_23/w25_04_23_a3.py
+++ 25_04_23/w25_04_23_a3.py
@@ -1,14 +1,10 @@
 vowel = ["a", "e", "i", "o", "u"]
 user_input = input("Enter a text : ").lower()
-
 isVowel = False
 for char in vowel:
     if char in user_input:
         isVowel = True
         break
-    else:
-        isVowel = False
-if isVowel:
-    print("There is vowel")
-else:
-    print("There is no vowel")
+    else: isVowel = False
+if isVowel: print("There is vowel")
+else: print("There is no vowel")
